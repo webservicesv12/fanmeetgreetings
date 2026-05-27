06e1a49ef3624dd6c0135ecc6cfcfa0ccbbddf03
Noah-V <noahvikoo3@gmail.com>
2026-05-27 20:36:31 +0100

    update

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "prisma/config";
 import "dotenv/config";
 
 export default defineConfig({
-  earlyAccess: true,
   schema: "prisma/schema.prisma",
   datasource: {
     url: process.env.DATABASE_URL!,
